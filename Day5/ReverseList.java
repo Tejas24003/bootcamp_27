81e2bf4410cf065e94fc2e402c5b38329f98ced7
Tejas Srivastava <162183581+Tejas24003@users.noreply.github.com>
2026-06-02 20:42:29 +0530

    Day5

--- Day5/ReverseList.java
+++ Day5/ReverseList.java
@@ -2,7 +2,21 @@ package bootcamp_27.Day5;
 import java.util.*;
 
 public class ReverseList {
-    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        LinkedList list =new LinkedList<>();
+        for(int i=0;i<n;i++){
+             list.add(sc.nextInt());
+        }
+        System.out.print(list +"\n");
+        Collections.reverse(list);
+        System.out.print("New list");
+        System.out.print(list);
+
+
+
+    }
 
 
 
