40bfe2052ece30bea544c8a276ebe8bad527edc7
Tejas Srivastava <162183581+Tejas24003@users.noreply.github.com>
2026-06-06 09:57:52 +0530

    Create Validp.java

---
+++ Day8/Validp.java
@@ -0,0 +1,14 @@
+package bootcamp_27.Day8;
+import java.util.*;
+public class Validp {
+    public static void main(String []args)
+    {
+          Scanner sc=new Scanner(System.in);
+           System.out.print("enter the number");
+         int a =sc.nextInt();
+           System.out.print(a);
+      
+    }
+  
+    
+}
